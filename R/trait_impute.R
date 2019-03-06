44811328fd692583df188eae5ea8de974548a338
richardjtelford <richard.telford@bio.uib.no>
2019-03-06 18:40:27 +0100

    added beginning of roxygen help

--- R/trait_impute.R
+++ R/trait_impute.R
@@ -1,6 +1,24 @@
 #' Impute traits
 #' @description Impute traits 
+#' @param comm community data in long format
+#' @param traits trait data in long format
+#' @param scale_hierarchy character vector of site/block/plot hierarchy (large to small)
+#' @param taxon_col character; name of taxon column in comm and traits
+#' @param trait_col character; name of trait name column in traits
+#' @param value_col character; name of trait value column in traits
+#' @param abundance_col character; name of species abundance column in comm
+#' @param other_col name of other grouping columns in comm
 #' 
+#' @description 
+#' 
+#' @return 
+#' 
+#' @importFrom stats sd var weighted.mean
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select one_of mutate group_by filter left_join n
+#' @importFrom purrr map_df
+#' @importFrom rlang !!! maybe_missing .data
+ 
 trait_impute <- function(comm, traits, scale_hierarchy = c("Country", "Site", "BlockID", "PlotID"), taxon_col = "taxon",  trait_col = "trait", value_col = "Value", abundance_col = "Cover", other_col = "Year"){
   
   out <- ((length(scale_hierarchy)):1) %>%  
@@ -11,17 +29,17 @@ trait_impute <- function(comm, traits, scale_hierarchy = c("Country", "Site", "B
       }
       scale_keep <- scale_hierarchy[1:.]
 
-     trait %>% select(-one_of(scale_drop))
+     traits %>% select(-one_of(scale_drop))
      comm %>% 
-        left_join(trait, by = c(scale_keep, taxon)) %>% 
-        group_by(!!!keep_scale, !!!taxon_col, !!!trait_col, !!!other_col) %>% 
-      mutate(weight = !!!cover_col/n()) %>% 
+        left_join(traits, by = c(scale_keep, taxon_col)) %>% 
+        group_by(!!!scale_keep, !!!taxon_col, !!!trait_col, !!!maybe_missing(other_col)) %>% 
+      mutate(weight = !!!abundance_col/n()) %>% 
 #      group_by(!!!keep_scale, !!!trait_col, !!!other_col) %>% 
       mutate(level = factor(scale_hierarchy[.], levels = scale_hierarchy, ordered = TRUE))
   }) %>% 
     filter(!is.na(!!!value_col)) %>% 
-    filter(level == min(level)) %>% 
-    group_by(!!!keep_scale, !!!trait_col, !!!other_col)
+    filter(level == min(.data$level)) %>% 
+    group_by(!!!scale_keep, !!!trait_col, !!!maybe_missing(other_col))
 
   class(out) <- "imputed_traits"
   }
@@ -29,15 +47,29 @@ trait_impute <- function(comm, traits, scale_hierarchy = c("Country", "Site", "B
 
 
 
+#' Bootstrap traits
+#' @description Bootstrap impute traits 
+#' @param traits_comm imputed trait and community data in long format
+#' @param nrep number of bootstrap replicates
+#' @param sample_size number of plants per sample
+#' @description 
+#' 
+#' @return 
+#' 
+#' @importFrom stats var 
+#' @importFrom e1071 skewness kurtosis
+#' @importFrom magrittr %>%
+#' @importFrom dplyr sample_n group_by summarise
+#' @importFrom purrr map_df
+#' @importFrom rlang !!! maybe_missing
+
+trait_bootstrap <- function(traits_comm, nrep = 100, sample_size = 200){  
+#  stopifnot(class(traits_com) == "imputed_traits")
   
-trait_bootstrap <- function(traits_comm){  
-  stopifnot(class(traits_com) == "imputed_traits")
-  
-  BootstrapMoments_All <- rerun(.n = nrep, sample_n(traits_comm, size = samplesize,  replace = TRUE, weight = traits_comm$weight)) %>%
-    bind_rows(.id = "n") %>% 
+  bootstrapMoments_All <- map_df(1:nrep, ~sample_n(traits_comm, size = sample_size,  replace = TRUE, weight = traits_comm$weight), .id = "n") %>% 
     group_by(n, add = TRUE) %>% 
     # get all the happy moments
-    summarise(Mean = mean(Value), Variance = var(Value), Skewness = skewness(Value), Kurtosis = kurtosis(Value))
+    summarise(mean = mean(Value), variance = var(Value), skewness = skewness(Value), kurtosis = kurtosis(Value))
   
-  return(BootstrapMoments_All)
+  return(bootstrapMoments_All)
 }
