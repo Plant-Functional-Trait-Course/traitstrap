7a8ea19478886916bdef178035dbefae1d90f1d9
richardjtelford <richard.telford@bio.uib.no>
2020-09-14 18:29:51 +0200

    improve processing in trait_impute_multi_level (almost there)

--- R/trait_impute_multi_level.R
+++ R/trait_impute_multi_level.R
@@ -1,7 +1,8 @@
-#' @importFrom purrr map_df set_names
+#' @importFrom purrr set_names map
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
-#' @importFrom dplyr filter mutate
+#' @importFrom dplyr filter mutate rename_with select bind_rows matches
+#' @importFrom stringr str_remove
 #'  
 trait_impute_multi_level <- function(call){
   # Find position of taxon_col
@@ -13,16 +14,20 @@ trait_impute_multi_level <- function(call){
   taxon_hierarchy <- set_names(taxon_hierarchy)
 
   #iterate over taxon_col
-  result <- map_df(taxon_hierarchy, ~{
+  result <- map(taxon_hierarchy, ~{
     # change taxon_col in call and eval 
     call[[taxon_position]] <- as.character(.x)
     eval(call)
-  }, .id = "taxon_level")
+  }) %>% 
+    map(select, -matches(paste0(taxon_hierarchy, "_trait"))) %>% 
+    map(rename_with, str_remove, matches(paste0(taxon_hierarchy, "_comm")), "_comm") %>% 
+    bind_rows(.id = "taxon_level")
    
   
   #filter lowest value of taxon hierarchy 
-  #warning - depends on grouping structure being correct!
+  # need to add species to group
   result <- result %>% 
+  #  group_by(.data[[taxon_hierarchy]], .add = TRUE) %>% 
     mutate(taxon_level = factor(.data$taxon_level, levels = taxon_hierarchy, ordered = TRUE)) %>% 
     filter(.data$taxon_level == min(.data$taxon_level))
   
