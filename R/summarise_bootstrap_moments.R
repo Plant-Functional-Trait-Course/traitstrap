897ad0ec7ca78ae7d76caf308887544e442b0904
richardjtelford <richard.telford@bio.uib.no>
2021-02-11 19:30:45 +0100

    with sd_mult * sd

--- R/summarise_bootstrap_moments.R
+++ R/summarise_bootstrap_moments.R
@@ -1,22 +1,30 @@
 #' Summarise Bootstrap traits
 #' @description Bootstraptraits
 #' @param bootstrap_moments trait moments from trait_np_bootstrap
+#' @param sd_mult Number of standard deviations around each moment,
+#' defaults to one
+#' @param ci Confidence interval - alternative to setting `sd_mult`
 
 #' @description
 #'
 #' @return tibble with the grouping variables and the mean of each moment
-#'  (+/- 1SD)
+#'  (+/- sd_mult * SD)
 #'
 #' @importFrom magrittr %>%
 #' @importFrom dplyr n group_by any_of summarise across rename
 #' @importFrom rlang .data
+#' @importFrom stats qnorm
 #' @export
 
-trait_summarise_boot_moments <- function(bootstrap_moments) {
+trait_summarise_boot_moments <- function(bootstrap_moments, sd_mult = 1, ci) {
   attrib <- attr(bootstrap_moments, "attrib")
   groups <- c(as.character(attrib$scale_hierarchy),
               attrib$trait_col,
               attrib$other_col)
+
+  if (!missing(ci)) {
+    sd_mult <- qnorm(1 - (1 - ci) / 2)
+  }
 
   # calculate means of moments
   summ_bootstrap_moments <- bootstrap_moments %>%
@@ -26,20 +34,20 @@ trait_summarise_boot_moments <- function(bootstrap_moments) {
     summarise(
       n = n(),
       mean = mean(.data$MEAN),
-      ci_low_mean = .data$mean - sd(.data$MEAN),
-      ci_high_mean = .data$mean + sd(.data$MEAN),
+      ci_low_mean = .data$mean - sd(.data$MEAN) * sd_mult,
+      ci_high_mean = .data$mean + sd(.data$MEAN) * sd_mult,
 
       var = mean(.data$variance),
-      ci_low_var = .data$var - sd(.data$variance),
-      ci_high_var = .data$var + sd(.data$variance),
+      ci_low_var = .data$var - sd(.data$variance) * sd_mult,
+      ci_high_var = .data$var + sd(.data$variance) * sd_mult,
 
       skew = mean(.data$skewness),
-      ci_low_skew = .data$skew - sd(.data$skewness),
-      ci_high_skew = .data$skew + sd(.data$skewness),
+      ci_low_skew = .data$skew - sd(.data$skewness) * sd_mult,
+      ci_high_skew = .data$skew + sd(.data$skewness) * sd_mult,
 
       kurt = mean(.data$kurtosis),
-      ci_low_kurt = .data$kurt - sd(.data$kurtosis),
-      ci_high_Kurt = .data$kurt + sd(.data$kurtosis)
+      ci_low_kurt = .data$kurt - sd(.data$kurtosis) * sd_mult,
+      ci_high_kurt = .data$kurt + sd(.data$kurtosis) * sd_mult 
     )
 
   return(summ_bootstrap_moments)
