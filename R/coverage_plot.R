afe8cc2f51ef8d13a104651f32509416637c645f
richardjtelford <richard.telford@bio.uib.no>
2021-02-24 13:49:29 +0100

    autoplot copes with other_cols

--- R/coverage_plot.R
+++ R/coverage_plot.R
@@ -1,5 +1,9 @@
 #' Coverage plot of Imputed Traits
 #' @param imputed_traits trait moments from trait_np_bootstrap
+#' @param other_col_how what to do with the other columns in other data.
+#'  Options are to `filter` by one of the columns, add them to the x-`axis`,
+#' `facet` by them, or to `ignore`.
+#' @param ... optional filters for use with `other_col_how = "filter"`
 #' @description Shows at which level the data are coming from in each plot.
 #'
 #' @return a ggplot
@@ -8,7 +12,9 @@
 #' @importFrom dplyr group_by summarise distinct bind_cols ungroup
 #' @importFrom rlang .data
 #' @importFrom ggplot2 autoplot ggplot geom_col facet_wrap aes
-#' @importFrom ggplot2 scale_y_continuous labs fortify
+#' @importFrom ggplot2 scale_y_continuous labs fortify facet_grid
+#' @importFrom stats reformulate
+#' @importFrom glue glue
 #' @examples
 #' require("ggplot2")
 #' data(community)
@@ -24,13 +30,38 @@ autoplot.imputed_trait <- function(imputed_traits, other_col_how, ...) {
   #get scale_hierarchy and concatenate to make an ID
   attrib <- attr(imputed_traits, "attrib")
   
-  if (length(attrib$other_col) > 0)
-
-  imputed_traits_summary <- fortify.imputed_trait(imputed_traits)
-  ggplot(imputed_traits_summary, aes(x = .data$.id,
+  if (length(attrib$other_col) > 0 & missing(other_col_how)) {
+    stop(
+      glue("Autoplot needs to know what to do with the other_col. Options are:
+            axis: add to x-axis labels
+            facet: add facets in the plot
+            filter: filter out one value of interest
+            ignore: ignore")
+    )
+  }
+  
+  if (!missing(other_col_how)) {
+    other_col_how <- match.arg(
+      arg = other_col_how, 
+      choices = c("axis", "facet", "filter", "ignore")
+    )
+  }
+  
+  imputed_traits_summary <- fortify.imputed_trait(imputed_traits, other_col_how, ...)
+  
+  plot <- ggplot(imputed_traits_summary, aes(x = .data$.id,
                                      y = .data$s, fill = .data$level)) +
     geom_col() +
     scale_y_continuous(expand = c(0, 0)) +
-    labs(x = "Plot", y = "Proportion of cover", fill = "Data source") +
-    facet_wrap(~ .data[[attrib$trait_col]])
+    labs(x = "Plot", y = "Proportion of cover", fill = "Data source")
+  
+  # add facets
+  if (!missing(other_col_how) && other_col_how == "facet"){
+    plot <- plot + 
+      facet_grid(reformulate(attrib$trait_col, attrib$other_col))
+  } else {
+   plot <- plot + facet_wrap(~ .data[[attrib$trait_col]])
+  }
+  
+  return(plot)
 }
--- R/fortify_impute_traits.R
+++ R/fortify_impute_traits.R
@@ -1,21 +1,40 @@
 #' Fortify Imputed Traits
-#' @param imputed_traits trait moments from trait_np_bootstrap
+#' @param imputed_traits imputed traits from trait_impute
+#' @param other_col_how what to do with the other columns in other data.
+#'  Options are to `filter` by one of the columns, add them to the x-`axis`,
+#'  `facet` by them, or to `ignore`.
+#' @param ... optional filters for use with `other_col_how = "filter"`
 #' @description Calculates at which level the data are coming from in each plot.
 #'
 #' @return a tibble
 #'
 #' @importFrom magrittr %>%
-#' @importFrom dplyr group_by summarise distinct bind_cols ungroup any_of
+#' @importFrom dplyr group_by summarise distinct bind_cols ungroup any_of 
+#' @importFrom dplyr filter 
 #' @importFrom rlang .data
 #' @importFrom ggplot2 fortify
 #' @export
 
 
-fortify.imputed_trait <- function(imputed_traits) {
+fortify.imputed_trait <- function(imputed_traits, other_col_how, ...) {
   #get scale_hierarchy and concatenate to make an ID
   attrib <- attr(imputed_traits, "attrib")
-  scale_hierarchy <- attrib$scale_hierarchy
+  scale_hierarchy <- as.character(attrib$scale_hierarchy)
   scale_hierarchy <- scale_hierarchy[scale_hierarchy != "global"]
+
+  if (!missing(other_col_how)) {
+    other_col_how <- match.arg(
+      arg = other_col_how, 
+      choices = c("axis", "facet", "filter", "ignore")
+    )
+    if (other_col_how == "filter") {
+      imputed_traits <- imputed_traits %>% filter(...)
+    }  
+    if (other_col_how == "axis") {
+      scale_hierarchy <- c(scale_hierarchy, attrib$other_col)
+    } 
+  }
+  
   id <- imputed_traits %>%
     ungroup() %>%
     select(any_of(scale_hierarchy)) %>%
@@ -24,7 +43,8 @@ fortify.imputed_trait <- function(imputed_traits) {
   imputed_traits_summary <- imputed_traits %>%
     bind_cols(.id = id) %>%
     group_by(
-      .data$.id, .data$level,
+      .data$.id,
+      .data$level,
       .data[[attrib$trait_col]], .data[[attrib$taxon_col]],
       .add = TRUE) %>%
     distinct(.data[[attrib$abundance_col]], .keep_all = TRUE) %>%
