90a459bcd325ddbd378866f01d9c5f6d87f349a7
bmaitner <bmaitner@gmail.com>
2021-02-24 16:56:31 -0500

    fixed issue in get_ci tests

--- tests/testthat/test-get_ci.R
+++ tests/testthat/test-get_ci.R
@@ -21,13 +21,15 @@ test_that("get_ci ranks are appropriate", {
 })
 
 test_that("get_ci methods return same class and type", {
-
+  test_vector <- runif(n = 100, min = 0, max = 100)
 expect_equal(object = typeof(get_ci(data = test_vector, which = "high")),
                expected = typeof(get_ci(data = test_vector, which = "high")))
 
 })
 
 test_that("get_ci data order doesn't matter", {
+
+  test_vector <- runif(n = 100, min = 0, max = 100)
 
   expect_equal(object = get_ci(data = test_vector[100:1], which = "high"),
                expected = get_ci(data = test_vector[1:100], which = "high"))
