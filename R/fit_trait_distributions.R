92fa3865a70ed312135702333aaa6d18f10c1e8f
bmaitner <bmaitner@gmail.com>
2021-02-25 16:28:12 -0500

    added a helper function to fit distributions, and a main function to implement it

---
+++ R/fit_trait_distributions.R
@@ -0,0 +1,114 @@
+#' Fit trait distributions
+#' @description Fits distributions to imputed traits and returns a tibble containining the fitted parameters.
+#' @param imputed_traits imputed trait and community data in long format
+#' @param distribution_type the type of statistical distribution to use. Character or named list. Currently accepts "normal","lognormal", and "beta".  
+#' @notes Distribution type should be a single character (e.g. "normal") or a named list (e.g. list(height="normal",mass="lognormal"))
+#' @description
+#'
+#' @return a tibble
+#'
+#' @importFrom stats var
+#' @importFrom e1071 skewness kurtosis
+#' @importFrom magrittr %>%
+#' @importFrom dplyr slice_sample group_by summarise
+#' @importFrom purrr map_dfr
+#' @importFrom stats var
+#' @importFrom fitdistrplus fitdist
+
+#' @examples 
+#' @export
+fit_trait_distributions(imputed_traits, distribution_type = "normal"){
+  
+  #Pull useful information from imputed traits object
+  value_col <- attributes(imputed_traits)$attrib$value_col
+  trait_col <- attributes(imputed_traits)$attrib$trait_col
+  taxon_col <- attributes(imputed_traits)$attrib$taxon_col
+  abundance_col <- attributes(imputed_traits)$attrib$abundance_col
+  scale_hierarchy <- attributes(imputed_traits)$attrib$scale_hierarchy
+  
+  
+  
+  #If only a single type of distribution was supplied, apply it to all traits
+  if(length(distribution_type)==1){
+    distributions <- unique(as.data.frame(imputed_traits)[,trait_col])
+    dist_list <- replicate(n = length(distributions),expr = distribution_type ) 
+    names(dist_list) <- distributions
+    distribution_type <- as.list(dist_list)
+    rm(dist_list,distributions)
+  }#if statement
+  
+  
+  #Data checks
+  
+  #check distribution type is supported
+  if (any(!unique(distribution_type)%in%c("beta","normal","lognormal"))) {
+    x = distribution_type[!distribution_type %in% c("beta","normal","lognormal")]
+    stop(glue("distribution type '{x}' not supported "))
+  }
+
+  
+
+  #Beta checks
+  if("beta" %in% distribution_type){
+    
+    
+    #make sure every species x hierarchy combination has at least 2 data points
+    beta_counts <- imputed_traits %>% 
+      filter(.data[[trait_col]] == names(distribution_type)[distribution_type=="beta"]) %>% 
+      group_by_at(c(as.character(scale_hierarchy),trait_col,taxon_col)) %>% 
+      summarise(n=n(),.groups="keep") 
+    
+    if(any(beta_counts$n < 2)) {
+    stop("Fitting a beta distrbution requires at least 2 points per distribution.
+         We suggest re-imputing traits with a minimum of (at least) 2 leaves.") 
+      }
+    
+    
+    #check that values are between 0 and 1
+    beta_vals <- imputed_traits %>% ungroup() %>%
+      filter(.data[[trait_col]] == names(distribution_type)[distribution_type=="beta"]) %>% select( all_of(value_col) )
+    
+    if(any(beta_vals > 1 | beta_vals < 0)){
+      stop("For a Beta distribution values must be between 0 and 1.")
+    }
+    
+    
+  }# end beta checks    
+  
+  
+  #lognormal checks
+  if("lognormal" %in% distribution_type){
+    
+    ln_vals <- imputed_traits %>% ungroup() %>%
+      filter(.data[[trait_col]] == names(distribution_type)[distribution_type=="lognormal"]) %>% select( all_of(value_col) )
+    
+    if(any(ln_vals <= 0)){
+      stop("For a lognormal distribution values must be positive.")
+    }
+  
+  }#end lognormal checks    
+  
+
+  # Main body
+
+  distribution_parms <- imputed_traits %>% 
+    group_by(.data[[taxon_col]], .add = TRUE) %>% 
+    mutate(distribution_type = unlist(distribution_type[.data[[trait_col]]]), fit_sample_size = n()) %>% 
+    summarize(fit_sample_size = n(), get_dist_parms(data = .data[[value_col]],distribution_type = distribution_type))
+  
+  #set arguments as attributes so next functions have access to them
+  attr(distribution_parms,"attrib") <- attributes(imputed_traits)
+  
+  class(distribution_parms) <- c("parametric_distrbutions", class(distribution_parms))
+  
+  return(distribution_parms)
+  
+  
+}# fx
+
+#################################################
+
+
+
+
+
---
+++ R/get_dist_parms.R
@@ -0,0 +1,70 @@
+#' Get distribution parameters
+#' @description Internal helper function for calculating parametric distribution parameters.
+#' @param data Numeric vector
+#' @param distribution_type Character.  One of either "normal", "lognormal", or "beta"
+#' @note This function will first try fitting using mme, then mle
+#' @importFrom fitdistrplus fitdist gofstat
+#' @keywords internal
+
+get_dist_parms <- function(data, distribution_type){
+  
+  
+  if(distribution_type=="normal"){
+    try( fit <- fitdist(data = data,distr = "norm",keepdata = F,method="mme"))
+    if(!exists("fit")){ fit <- fitdist(data = data,distr = "norm",keepdata = F,method="mle") }
+    
+    gof <- gofstat(fit)  
+    return(data.frame(distribution_type="normal",
+                      parm1 = fit$estimate[[1]],
+                      parm2 = fit$estimate[[2]],
+                      ks= gof$ks,
+                      cvm = gof$cvm,
+                      ad = gof$ad)
+    )
+    
+    
+  }
+  
+  
+  if(distribution_type == "lognormal"){
+    try( fit <- fitdist(data = data,distr = "lnorm",keepdata = F,method="mme"))
+    if(!exists("fit")){ fit <- fitdist(data = data,distr = "lnorm",keepdata = F,method="mle") }
+    
+    gof <- gofstat(fit)  
+    return(data.frame(distribution_type="lognormal",
+                      parm1 = fit$estimate[[1]],
+                      parm2 = fit$estimate[[2]],
+                      ks= gof$ks,
+                      cvm = gof$cvm,
+                      ad = gof$ad)
+    )
+    
+    
+  }
+  
+  
+  if(distribution_type=="beta"){
+    try( fit <- fitdist(data = data,distr = "beta",keepdata = F,method="mme"))
+    if(!exists("fit")){ fit <- fitdist(data = data,distr = "beta",keepdata = F,method="mle") }
+    gof <- gofstat(fit)  
+    return(data.frame(distribution_type="beta",
+                      parm1 = fit$estimate[[1]],
+                      parm2 = fit$estimate[[2]],
+                      ks= gof$ks,
+                      cvm = gof$cvm,
+                      ad = gof$ad)
+    )
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
